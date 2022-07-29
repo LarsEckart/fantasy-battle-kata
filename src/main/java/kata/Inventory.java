1f4ff8e8fc179c0028c29380e20b67dbd7ecf36b
Lars Eckart <lars.eckart@hey.com>
2022-07-29 14:50:54 +0300

    another TODO

--- src/main/java/kata/Inventory.java
+++ src/main/java/kata/Inventory.java
@@ -3,6 +3,9 @@ package kata;
 public class Inventory {
 
     private final Equipment equipment;
+    // TODO: besides equipped items, players can have a bag which can contain charms, which can be offensive or defensive
+    //  "Forked Charm", damage modifier: 2.0
+    //  "Stalwart Charm", soak modifier: 8.5
 
     public Inventory(Equipment equipment) {
         this.equipment = equipment;
