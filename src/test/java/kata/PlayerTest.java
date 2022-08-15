a6ec9a2bbb4fca1f9a6e313aa61de13cd6116176
Lars Eckart <lars.eckart@hey.com>
2022-08-15 12:56:52 +0300

    tests with and without mocks

--- src/test/java/kata/PlayerTest.java
+++ src/test/java/kata/PlayerTest.java
@@ -1,31 +1,66 @@
 package kata;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
 
-    @Disabled("Test is not finished yet")
     @Test
     void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
+        Equipment equipment = mock(Equipment.class);
+        Weapon mainHand = mock(Weapon.class);
+        Weapon offHand = mock(Weapon.class);
+        Jewellery necklace = mock(Jewellery.class);
         Stats stats = mock(Stats.class);
         RareMob target = mock(RareMob.class);
+        Armor armor = mock(Armor.class);
+        DefensiveBuff defensiveBuff = mock(DefensiveBuff.class);
 
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        given(inventory.getEquipment()).willReturn(equipment);
+        given(equipment.getRightHand()).willReturn(mainHand);
+        given(equipment.getLeftHand()).willReturn(offHand);
+        given(equipment.getNecklace()).willReturn(necklace);
+        given(mainHand.getBaseDamage()).willReturn(18);
+        given(mainHand.getDamageModifier()).willReturn(3.5f);
+        given(offHand.getDamageModifier()).willReturn(3.7f);
+        given(offHand.getBaseDamage()).willReturn(15);
+        given(necklace.getDamageModifier()).willReturn(2.9f);
+        given(stats.getStrength()).willReturn(18);
+        given(target.getArmor()).willReturn(armor);
+        given(armor.getType()).willReturn(ArmorType.MAIL);
+        given(armor.getDamageSoak()).willReturn(21);
+        given(target.getBuffs()).willReturn(List.of(defensiveBuff));
+        given(defensiveBuff.soakModifier()).willReturn(5.0f);
+        Player player = new Player(inventory, stats);
+
+        Damage damage = player.calculateDamage(target);
+
+        assertEquals(431, damage.getAmount());
     }
 
-    @Disabled("Test is not finished yet")
+
     @Test
     void damageCalculations() {
-        Inventory inventory = new Inventory(null);
-        Stats stats = new Stats(0);
-        RareMob target = new RareMob(null);
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        Inventory inventory = new Inventory(
+                new Equipment(
+                        new CommonWeapon(null, 18, 3.5f),
+                        new CommonWeapon(null, 15, 3.7f),
+                        null,
+                        null,
+                        null,
+                        new Jewellery(null, 2.9f)));
+        Stats stats = new Stats(18);
+        RareMob target = new RareMob(new CommonArmor(null, 21, ArmorType.MAIL));
+        Player player = new Player(inventory, stats);
+
+        Damage damage = player.calculateDamage(target);
+
+        assertEquals(431, damage.getAmount());
     }
 }
