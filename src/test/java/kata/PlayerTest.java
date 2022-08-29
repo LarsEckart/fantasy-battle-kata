dfb6ea45255ad131492220f36db2a908c8855005
Lars Eckart <lars.eckart@hey.com>
2022-08-29 08:49:55 +0300

    already use values from readme in prepared test

--- src/test/java/kata/PlayerTest.java
+++ src/test/java/kata/PlayerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
@@ -13,19 +14,20 @@ public class PlayerTest {
     void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
+        given(stats.getStrength()).willReturn(18);
         RareMob target = mock(RareMob.class);
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(431, damage.getAmount());
     }
 
     @Disabled("Test is not finished yet")
     @Test
     void damageCalculations() {
         Inventory inventory = new Inventory(null);
-        Stats stats = new Stats(0);
+        Stats stats = new Stats(18);
         RareMob target = new RareMob(null);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(431, damage.getAmount());
     }
 }
